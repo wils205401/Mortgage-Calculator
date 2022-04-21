d1e905306ada1f519c167fbb977ebdb0cefac7d0
Wilson <wilson.leung.2054@hotmail.com>
2022-04-21 20:37:38 +0100

    Added functionality for calculating payments per period

--- mortgage.py
+++ mortgage.py
@@ -5,12 +5,24 @@
 # import packages
 import pandas as pd
 import numpy as np
+import datetime
 
-x = 2
+def mortgage_payments(loan_amt, maturity, payments_per_year, interest, option=1, start_date=None):
+    interest_per_period = interest / payments_per_year
+    number_of_payments = payments_per_year * maturity
 
-def mortage_payments(loam_amt, maturity, payments_per_year, interest, excel=0):
+    # Formula for Payment per Period (e.g. monthly payments)
+    payments_per_period = (interest_per_period*loan_amt) / (1-(1+interest_per_period)**(-1*number_of_payments))
 
-    # if excel == 1:
+    if option == 1:
+        return payments_per_period
+
+    # start date is a string, convert to datetime
+    start_date = datetime.datetime.strptime(start_date, '%Y/%m/%d').date()
+
+
+
+    # if option == 3:
         # df.to_csv('mortgage_payments_schedule.csv')
     pass
 
@@ -25,10 +37,32 @@ def main():
     # payment frequency
     PaymentsPerYear = float(input('Enter the number of payments per year: '))
     # interest rate
-    InterestRate = float(input('Enter the annual interest rate on your loan: '))
+    InterestRate = float(input('Enter the annual interest rate on your loan (decimals): '))
+    print('-----------------------------')
+    options = int(input('''Please input the operation you wish to perform.
+(1) Calculate your payment per period
+(2) Get a detailed summary of your payment schedule
+(3) Get a detailed summary of your payment schedule and save it as an Excel file
+Enter (1/2/3): '''))
 
-    options = input('Please input the operation you wish to perform. 1 for ')
+    if options == 1:
+        payment_per_period = mortgage_payments(LoanAmount, Maturity, PaymentsPerYear, InterestRate, options)
+        print(f"${round(payment_per_period, 2)}")
+    elif options == 2:
+        start_date = input('Enter the start date of mortgage (YYYY/MM/DD): ')
+        pass
+
+    elif options == 3:
+        start_date = input('Enter the start date of mortgage (YYYY/MM/DD): ')
+        pass
+
+    else:
+        raise ValueError('Please enter a valid number. (1/2/3)')
 
 
 if __name__ == '__main__':
     main()
+
+
+# unit tests to validate correct calculations
+# answers are obtained from other online mortgage calculators
